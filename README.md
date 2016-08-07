3ae0d9c52fa6d424383f1e50fdc2c77272adb056
Carlo Panzi <carlo.panzi@gmail.com>
2016-08-07 14:44:08 +0200

    api working

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # inshort
+
+## API
+
+Retrieve a shortened url
+
+**POST** http://localhost:3000/new
+
+{
+url: ...
+}
+
+
+
+Redirect to the page
+
+http://localhost:3000/<token>
--- shortener.js
+++ shortener.js
@@ -10,16 +10,35 @@ const mongoUri = process.env.MONGOURI;
 // If you're testing this locally, change mongoUri to:
 // 'mongodb://localhost:27017/shortio'
 
+console.log(mongoUri);
 
 const options = {  
-  server: {
-    socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
-  }, 
-  replset: {
-    socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 }
-  }
+	server: {
+		socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
+	}, 
+	replset: {
+		socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 }
+	}
 };
 
 mongoose.connect(mongoUri, options);
 
 const db = mongoose.connection; 
+
+server.connection({  
+	port: process.env.PORT || 3000,
+	routes: { cors: true }
+});
+
+server.register(require('inert'), (err) => {  
+	db.on('error', console.error.bind(console, 'connection error:'))
+	.once('open', () => {
+		server.route(routes);
+
+		server.start(err => {
+			if (err) throw err;
+
+			console.log(`Server running at port ${server.info.port}`);
+		});
+	});
+});
