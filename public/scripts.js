fe78d2640366d3d5c0fbc6611558aff7c4588a15
Carlo <carlo.panzi@gmail.com>
2020-04-14 21:43:58 +0200

    tt
    tests so far
    
    Removed light server
    
    Removed nanoajax
    
    fix post
    
    get redirect function
    
    Removed broken pattern
    
    redirects
    
    Auth
    
    auth

--- public/scripts.js
+++ public/scripts.js
@@ -1,47 +1,46 @@
 // Cache DOM elements in memory
 var form = document.getElementById("shorten-form");
-var urlBox = form.elements[0];
+var urlBox = document.querySelector('input[name="url"]');
+var authBox = document.querySelector('input[name="auth"]');
 var link = document.getElementById("link");
 var shrBox = document.getElementById("shortened");
 
+var postEndpoint = "/.netlify/functions/post";
+
 function displayShortenedUrl(response) {
-	link.textContent = response.shortUrl;
-	link.setAttribute("href", response.shortUrl);
+	const url = `${location.href}r/${response.hash}`;
+	link.textContent = url;
+	link.setAttribute("href", url);
 	shrBox.classList += " visible";
-	urlBox.value = "";
+	//urlBox.value = "";
 }
 
 const errorMessages = {
 	500: "Oops, error on our side",
+	400: "Missing URL",
+	401: "Not authorized",
 	default:
-		"Are you sure the URL is correct? Make sure it has http:// at the beginning."
+		"Are you sure the URL is correct? Make sure it has http:// at the beginning.",
 };
 
-function alertError(code, error) {
+function alertError(code) {
 	alert(errorMessages[code] || errorMessages.default);
 }
 
-function appendHttp(url) {
-	if (url.match(/^www\./i)) {
-		return "http://" + url;
-	}
-	return url;
-}
-
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
 	event.preventDefault();
-
-	nanoajax.ajax(
-		{
-			url: "/new",
-			method: "POST",
-			body: "url=" + appendHttp(urlBox.value)
-		},
-		(code, responseText, request) => {
-			if (code !== 200) {
-				return alertError(code, responseText);
-			}
-			displayShortenedUrl(JSON.parse(responseText));
+	fetch(postEndpoint, {
+		method: "POST",
+		body: JSON.stringify({ url: urlBox.value, apiKey: authBox.value }),
+	}).then((response) => {
+		if (response.status >= 400) {
+			/* if (response.body) {
+				return response.json().then((data) => {
+					console.log("eeoe", data);
+				});
+			} */
+			return alertError(response.status);
 		}
-	);
+		return response.json().then(displayShortenedUrl);
+	});
 });
