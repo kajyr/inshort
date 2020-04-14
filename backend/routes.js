6e4bd97c1eb616da3ac3b0383d029006b2b58f65
Carlo <carlo.panzi@gmail.com>
2020-04-14 21:43:58 +0200

    refactor: Back to mongo / docker

---
+++ backend/routes.js
@@ -0,0 +1,76 @@
+const { nanoid } = require('nanoid');
+
+const format = ({ hash, createdAt, url }) => ({ hash, createdAt, url });
+
+const routes = [
+  {
+    method: 'POST',
+    url: '/new',
+    schema: {
+      type: 'object',
+      required: ['url', 'apiKey'],
+      properties: {
+        url: { type: 'string' },
+        apiKey: { type: 'string' },
+      },
+    },
+    handler: async function (request, reply) {
+      const { url, apiKey } = request.body;
+      if (!apiKey || apiKey !== process.env.API_KEY) {
+        reply.code(401).send();
+        return;
+      }
+      if (!url) {
+        reply.code(400).send();
+        return;
+      }
+      if (!url.match(/^http[s]?:\/\//)) {
+        reply.code(405).send();
+        return;
+      }
+      const collection = await this.mongo.db.collection('urls');
+      const found = await collection.findOne({ url });
+
+      if (found) {
+        return format(found);
+      }
+
+      const uniqueID = nanoid(6);
+
+      const newUrl = {
+        hash: uniqueID,
+        url,
+        createdAt: new Date(),
+      };
+
+      await collection.insertOne(newUrl);
+
+      return format(newUrl);
+    },
+  },
+  {
+    method: 'GET',
+    path: '/:hash',
+    handler: async function (request, reply) {
+      const { hash } = request.params;
+      const collection = await this.mongo.db.collection('urls');
+
+      const found = await collection.findOne({ hash });
+
+      if (found) {
+        reply.redirect(301, found.url);
+        return;
+      }
+      reply.redirect(`${request.protocol}://${request.hostname}/404.html`);
+      return;
+    },
+  },
+];
+
+module.exports = function (fastify, opts, done) {
+  routes.forEach((route) => {
+    fastify.route(route);
+  });
+
+  done();
+};
--- public/scripts.js
+++ public/scripts.js
@@ -1,47 +1,44 @@
 // Cache DOM elements in memory
-var form = document.getElementById("shorten-form");
-var urlBox = form.elements[0];
-var link = document.getElementById("link");
-var shrBox = document.getElementById("shortened");
+var form = document.getElementById('shorten-form');
+var urlBox = document.querySelector('input[name="url"]');
+var authBox = document.querySelector('input[name="auth"]');
+var link = document.getElementById('link');
+var shrBox = document.getElementById('shortened');
+
+var postEndpoint = '/new';
 
 function displayShortenedUrl(response) {
-	link.textContent = response.shortUrl;
-	link.setAttribute("href", response.shortUrl);
-	shrBox.classList += " visible";
-	urlBox.value = "";
+  const url = `${location.href}${response.hash}`;
+  link.textContent = url;
+  link.setAttribute('href', url);
+  shrBox.classList += ' visible';
+  //urlBox.value = "";
 }
 
 const errorMessages = {
-	500: "Oops, error on our side",
-	default:
-		"Are you sure the URL is correct? Make sure it has http:// at the beginning."
+  500: 'Oops, error on our side',
+  400: 'Missing URL',
+  401: 'Not authorized',
+  default: 'Are you sure the URL is correct? Make sure it has http:// at the beginning.',
 };
 
-function alertError(code, error) {
-	alert(errorMessages[code] || errorMessages.default);
+function alertError(code) {
+  alert(errorMessages[code] || errorMessages.default);
 }
 
-function appendHttp(url) {
-	if (url.match(/^www\./i)) {
-		return "http://" + url;
-	}
-	return url;
-}
-
-form.addEventListener("submit", function(event) {
-	event.preventDefault();
-
-	nanoajax.ajax(
-		{
-			url: "/new",
-			method: "POST",
-			body: "url=" + appendHttp(urlBox.value)
-		},
-		(code, responseText, request) => {
-			if (code !== 200) {
-				return alertError(code, responseText);
-			}
-			displayShortenedUrl(JSON.parse(responseText));
-		}
-	);
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  fetch(postEndpoint, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url: urlBox.value, apiKey: authBox.value }),
+  }).then((response) => {
+    if (response.status >= 400) {
+      return alertError(response.status);
+    }
+    return response.json().then(displayShortenedUrl);
+  });
 });
