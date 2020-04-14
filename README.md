afc9e535ff1046072474af5b54248f04beae950d
Carlo <carlo.panzi@gmail.com>
2020-04-14 10:16:51 +0200

    build: Upgrade deps

--- README.md
+++ README.md
@@ -10,19 +10,6 @@ Retrieve a shortened url
 url: ...
 }
 
-
-
 Redirect to the page
 
-<<<<<<< HEAD
 http://localhost:3000/<token>
-
-
-## Thanks
-
-to this beautiful tutorial :)
-
-http://codetuts.tech/build-a-url-shortener-node-hapi-js
-=======
-http://localhost:3000/rk_YniNF
->>>>>>> master
--- conf.js
+++ conf.js
@@ -1,11 +1,11 @@
-require('dotenv').load();
+require("dotenv").load();
 
-const PORT			= process.env.PORT || 3000
-const BASE_URL   	= process.env.BASE_URL || 'http://localhost:' + PORT;
-const MONGO_URI	= process.env.MONGOURI;  
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || "http://localhost:" + PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
 module.exports = {
-	MONGO_URI:MONGO_URI,
+	MONGO_URI: MONGO_URI,
 	PORT: PORT,
 	BASE_URL: BASE_URL
-}
+};
