7f16ff2252790f89b016ca96fdb6111c478fda4f
Carlo <carlo.panzi@gmail.com>
2020-04-20 15:25:19 +0200

    get redirect function

--- functions/get.js
+++ functions/get.js
@@ -1,14 +1,16 @@
 const { MongoClient } = require("mongodb");
 
-exports.handler = async event => {
-	const url = event.queryStringParameters.url;
+exports.handler = async (event, context) => {
+	const hash = event.queryStringParameters.h;
 	// if no url...
-	if (!url) {
+	if (!hash) {
 		return {
 			statusCode: 400,
-			body: "Missing URL"
+			body: "Missing hash"
 		};
 	}
+
+	context.callbackWaitsForEmptyEventLoop = false;
 
 	const connection = await MongoClient.connect(process.env.MONGO_URI, {
 		useUnifiedTopology: true
@@ -17,7 +19,7 @@ exports.handler = async event => {
 	const db = connection.db(process.env.MONGO_DB);
 	const collection = db.collection("redirs");
 
-	const redisObject = await collection.findOne({ url });
+	const redisObject = await collection.findOne({ hash });
 
 	if (!redisObject) {
 		return {
@@ -26,8 +28,15 @@ exports.handler = async event => {
 		};
 	}
 
-	return {
+	/* return {
 		statusCode: 200,
 		body: JSON.stringify(redisObject, undefined, 2)
+	}; */
+
+	return {
+		statusCode: 301,
+		headers: {
+			Location: redisObject.url
+		}
 	};
 };
